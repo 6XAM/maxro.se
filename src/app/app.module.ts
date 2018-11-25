aedc960ca1b40e4e3a4d2437424de42f38970033
Max Rosenzweig <rosenzweig.max@gmail.com>
2018-11-25 00:05:50 -0500

    Beginning to add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TerminalComponent } from './terminal/terminal.component';
+import { AppTerminalButtonComponent } from './app-terminal-button/app-terminal-button.component';
+import { TerminalButtonComponent } from './terminal-button/terminal-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TerminalComponent,
+    AppTerminalButtonComponent,
+    TerminalButtonComponent
   ],
   imports: [
     BrowserModule
